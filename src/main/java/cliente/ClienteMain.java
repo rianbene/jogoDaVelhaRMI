f255404c3bf263b9569141963b04385ddfb34f3a
Rian Benedito <rian.bene14@gmail.com>
2026-05-21 18:26:16 -0300

    Update ClienteMain.java
    Add lógica de busca no servidor RMI na rede, instanciar o cliente e iniciar o loop de input

--- src/main/java/cliente/ClienteMain.java
+++ src/main/java/cliente/ClienteMain.java
@@ -1,4 +1,26 @@
 package cliente;
 
+import interfaces.GameServidor;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class ClienteMain {
+    static void main() {
+        try {
+            // procurar o registro RMI na rede. passar IP e porta
+            Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+
+            // buscar do servidor no registry pelo nome
+            GameServidor servidor = (GameServidor) registry.lookup("JogoDaVelhaServidor");
+
+            // instanciar o cliente passando a referencia do servidor
+            GameClienteImpl cliente = new GameClienteImpl(servidor);
+
+            // iniciar o loop dos inputs
+            cliente.iniciarInputLoop();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
